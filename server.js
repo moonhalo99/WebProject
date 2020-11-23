a07fc67c42998484b73775135fe24d680817823b
moonhalo99 <moonhalo99@naver.com>
2020-11-23 20:42:46 +0900

    start project

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/Pages')); // 1
+
+var port = 8001;
+app.listen(port, function(){
+  console.log('Server is running...\nAddress: http://localhost:'+port);
+});
